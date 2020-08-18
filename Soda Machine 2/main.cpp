1ed6447eba725db8e7ccd563440508ec57c6040d
Kyle Lovely <kylesphere@gmail.com>
2020-08-18 14:40:34 -0400

    Initial Commit

---
+++ Soda Machine 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Soda Machine 2
+//
+//  Created by Kyle Lovely on 8/18/20.
+//  Copyright © 2020 Kyle Lovely. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
